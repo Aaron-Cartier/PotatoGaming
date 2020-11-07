6cfbf2447ae0de91d9cf49f5c29cda9eb6fbde9b
Aaron-Cartier <aaron.cartier1@gmail.com>
2020-11-07 17:00:14 -0500

    commit 01 - added a layout for locally stored database for the app, aka customer page, here we will be able to enter customer info as well as view the info

--- app/src/main/java/com/example/potatogaming/MainActivity.java
+++ app/src/main/java/com/example/potatogaming/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.potatogaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button showCustomerPage = findViewById(R.id.btn_CustomerPage);
+        Log.d(TAG, "Creating Customer Page...");
+        showCustomerPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, localDatabase.class));
+            }
+        });
     }
 }
