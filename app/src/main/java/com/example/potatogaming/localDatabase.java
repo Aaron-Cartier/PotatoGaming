0d8fb1266f9a39cad33426a34e8220cb768e72b0
Aaron-Cartier <aaron.cartier1@gmail.com>
2020-11-08 02:08:26 -0500

    commit 04 - local database implemented, CRUD works 100%, will work on modifying the app's home page now..

--- app/src/main/java/com/example/potatogaming/localDatabase.java
+++ app/src/main/java/com/example/potatogaming/localDatabase.java
@@ -2,13 +2,19 @@ package com.example.potatogaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class localDatabase extends AppCompatActivity {
 
@@ -16,6 +22,8 @@ public class localDatabase extends AppCompatActivity {
     EditText et_name, et_age;
     Switch sw_activeCustomer;
     ListView lv_customerList;
+    ArrayAdapter customerArrayAdapter;
+    DataBaseLocal dataBaseLocal = new DataBaseLocal(localDatabase.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +37,54 @@ public class localDatabase extends AppCompatActivity {
         sw_activeCustomer = findViewById(R.id.sw_Active);
         lv_customerList = findViewById(R.id.lv_customerList);
 
+        final DataBaseLocal dataBaseLocal = new DataBaseLocal(localDatabase.this);
+        ShowCustomersOnListView(dataBaseLocal);
+
         btn_Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                CustomerModel customerModel;
                 try {
-                    CustomerModel customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
-                }
-                catch(Exception e){
+                    customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+                } catch (Exception e) {
                     Toast.makeText(localDatabase.this, "Please fill out all fields", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1, "error", 0, false);
                 }
+
+                DataBaseLocal dataBaseLocal = new DataBaseLocal(localDatabase.this);
+
+                boolean success = dataBaseLocal.addOne(customerModel);
+
+                Toast.makeText(localDatabase.this, "Success = " + success, Toast.LENGTH_SHORT).show();
+                ShowCustomersOnListView(dataBaseLocal);
+
             }
         });
 
         btn_Viewall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                DataBaseLocal dataBaseLocal = new DataBaseLocal(localDatabase.this);
+                //List<CustomerModel> everyone = dataBaseLocal.getEveryone();
+
+                //Toast.makeText(localDatabase.this, everyone.toString(), Toast.LENGTH_SHORT).show();
+                ShowCustomersOnListView(dataBaseLocal);
 
             }
         });
+
+        lv_customerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CustomerModel clickedCustomer = (CustomerModel) parent.getItemAtPosition(position);
+                dataBaseLocal.deleteOne(clickedCustomer);
+                ShowCustomersOnListView(dataBaseLocal);
+                Toast.makeText(localDatabase.this, "Customer Deleted" + clickedCustomer.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void ShowCustomersOnListView(DataBaseLocal dataBaseLocal) {
+        customerArrayAdapter = new ArrayAdapter<CustomerModel>(localDatabase.this, android.R.layout.simple_list_item_1, dataBaseLocal.getEveryone());
+        lv_customerList.setAdapter(customerArrayAdapter);
     }
 }
