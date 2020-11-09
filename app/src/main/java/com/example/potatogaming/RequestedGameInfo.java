1d6c7c1959739a22747d7064ce64c2d7dd38cccc
Aaron-Cartier <aaron.cartier1@gmail.com>
2020-11-09 00:34:57 -0500

    commit 07 - the on click functionality for each card in the requested game list page works, clicking on a card will open another activity showing just the card with its info. The Image isn't loading, will work on that next.

--- app/src/main/java/com/example/potatogaming/RequestedGameInfo.java
+++ app/src/main/java/com/example/potatogaming/RequestedGameInfo.java
@@ -3,12 +3,33 @@ package com.example.potatogaming;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RequestedGameInfo extends AppCompatActivity {
+    ImageView gmImage;
+    TextView gmTitle, gmDescription, gmPlatform, gmDeveloper, gmPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requested_game_info);
+
+        gmTitle = (TextView)findViewById(R.id.ivTitle);
+        gmDescription = (TextView)findViewById(R.id.ivDescription);
+        gmPlatform = (TextView)findViewById(R.id.ivPlatform);
+        gmDeveloper = (TextView)findViewById(R.id.ivDeveloper);
+        gmPrice = (TextView)findViewById(R.id.ivPrice);
+
+        Bundle gBundle = getIntent().getExtras();
+
+        if(gBundle != null) {
+            gmTitle.setText(gBundle.getString("Title"));
+            gmDescription.setText(gBundle.getString("Description"));
+            gmPlatform.setText(gBundle.getString("Platform"));
+            gmDeveloper.setText(gBundle.getString("Developer"));
+            gmPrice.setText(gBundle.getString("Price"));
+        }
+
     }
 }
